6167df595c98f10f28acf3d89141f84eb55fae95
sunny <jjohye11@gmail.com>
2020-10-04 12:59:37 +0900

    프로젝트2 진행사항 기록
    SPA 초기 로딩 처리하기

--- whois/start/src/App.js
+++ whois/start/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'antd/dist/antd.css';
 import { Route } from 'react-router-dom';
 import User from './user/container/User';
 import Search from './search/container/Search';
 
 export default function App() {
+  useEffect(() => {
+    const bodyEl = document.getElementsByTagName('body')[0];
+    const loadingEl = document.getElementById('init-loading');
+    bodyEl.removeChild(loadingEl);
+  }, []);
+  
   return (
     <>
     <Route exact path="/" component={Search}></Route>
